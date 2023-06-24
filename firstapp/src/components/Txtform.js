5e1a4b0eb50b9b65af3596592f987c415cbcdaf4
thisisZahid <72434947+Zahid40@users.noreply.github.com>
2023-06-24 19:17:03 +0530

    .

--- firstapp/src/components/Txtform.js
+++ firstapp/src/components/Txtform.js
@@ -20,6 +20,14 @@ export default function Txtform(props) {
   const hadleclear = ()=>{
     setText("")
   }
+  const handlespeakclick = ()=>{
+    let msg = new SpeechSynthesisUtterance();
+    msg.text = text
+    window.speechSynthesis.speak(msg)
+  }
+  const handlespeakstop = ()=>{
+    window.speechSynthesis.cancel()
+  }
 
   return (
     <>
@@ -27,14 +35,21 @@ export default function Txtform(props) {
     <div className="mb-3">
      <label htmlFor="My-Text" className="form-label">{props.lable}</label>
     <textarea className="form-control" id="exampleFormControlTextarea1" rows="10" value={text} onChange={handleonchange}></textarea>
+    <button className="btn btn-danger my-2 mx-2" onClick={hadleclear}>Clear</button>
+    <button className="btn btn-success my-2 mr-2" onClick={handlespeakclick}>Speak</button>
+    <button className="btn btn-warning my-2 mr-2" onClick={handlespeakstop}>Stop</button>
     <button className="btn btn-primary my-2 mx-2" onClick={handleupclick}>Convert to Uppercase</button>
     <button className="btn btn-primary mx-2 my-2" onClick={handlelowclick}>Convert to Lowercase</button>
-    <button className="btn btn-danger my-2 mx-2" onClick={hadleclear}>Clear</button>
     </div>
     <div className="container">
-      <h1>Your Text summary</h1>
-      <p clasName="b">Words : {text.split(" ").length} and characters : {text.length}</p>
-      <p>{ 0.005*text.split(" ").length} Minutes To Read</p>
+      <h2>Your Text summary</h2>
+      <p>Words : <strong>{text.split(" ").length}</strong></p>
+      <p>characters : <strong>{text.length}</strong></p>
+      <p>Sentence : <strong>{text.split(".").length}</strong></p>
+      <p>Paragraph : <strong>{text.split("\n\n").length}</strong></p>
+
+      <p><strong>{ 0.005*text.split(" ").length}</strong> Minutes To Read</p>
+      <p><strong>{text.length/text.split(" ").length}</strong> Charactors Per Word</p>
       <h2>Preview</h2>
       <p>{text}</p>
     </div>
--- firstapp/src/components/navbar.js
+++ firstapp/src/components/navbar.js
@@ -19,20 +19,6 @@ export default function Navbar(props) {
               <li className="nav-item">
                 <a className="nav-link" href="/">About SuperText</a>
               </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="/">Action</a></li>
-                  <li><a className="dropdown-item" href="/">Another action</a></li>
-                  <li><hr className="dropdown-divider" /></li>
-                  <li><a className="dropdown-item" href="/">Something else here</a></li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a href="/" className="nav-link disabled">Disabled</a>
-              </li>
             </ul>
             <form className="d-flex" role="search">
               <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
