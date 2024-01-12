1b836ec5fdeb5870c3d7b5e10eef16a41821cbda
thisisZahid <72434947+Zahid40@users.noreply.github.com>
2024-01-13 00:08:47 +0530

    130124

---
+++ portfolio-design-2024/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://zahid40.github.io/images/profile_img_3.png" className="App-logo" alt="logo" />
+        <p>
+          Helicopter Helicopter
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React in Helicopter
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
