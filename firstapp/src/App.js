4ae8d10a22307ca93c8dba5a6aebb1b298769017
thisisZahid <72434947+Zahid40@users.noreply.github.com>
2024-01-14 16:18:14 +0530

    140124

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,17 +1,19 @@
 import { useState } from 'react';
-// import AAbout from './components/About';
+import About from './components/About';
 import Txtform from './components/Txtform';
 import Navbar from './components/navbar';
 import './App.css';
 import Aleart from './components/Aleart';
+import { Route, Routes } from 'react-router-dom';
+
 
 
 function App() {
   const [mode, setmode] = useState('light');
 
-  const [aleart,setAleart] = useState(null);
-  const showalt = (type,message)=>{
-    setAleart({type,message})
+  const [aleart, setAleart] = useState(null);
+  const showalt = (type, message) => {
+    setAleart({ type, message })
     setTimeout(() => {
       setAleart(null)
     }, 1500);
@@ -27,29 +29,29 @@ function App() {
 
     if (document.documentElement.getAttribute('data-bs-theme') === 'light') {
       document.documentElement.setAttribute('data-bs-theme', 'dark');
-      showalt("success","Dark-Mode Enabled")
+      showalt("success", "Dark-Mode Enabled")
     }
     else {
       document.documentElement.setAttribute('data-bs-theme', 'light')
-      showalt("success","Light-Mode Enabled")
+      showalt("success", "Light-Mode Enabled")
     }
   }
-
-  
-
-
-
 
   return (
     <>
       <Navbar title="SuperText" mode={mode} toogleMode={toogleMode} />
-      <div className=".container-fluid" style={{backgroundColor : mode==='light'?'#c0d6e4':'#05141a' , height : '100%'}}>
-        <div className='container divalt'>
-          <Aleart aleart={aleart}/>
+        <div className=".container-fluid" style={{ backgroundColor: mode === 'light' ? '#c0d6e4' : '#05141a', height: '100%' }}>
+          <div className='container divalt'>
+            <Aleart aleart={aleart} />
+          </div>  
         </div>
-        <Txtform showalt={showalt} heading="Enter the Text to analyze............" mode={mode}/>
-        {/* <AAbout /> */}
-      </div>
+      <Routes>
+        
+        <Route path="/" element={<Txtform showalt={showalt} heading="Enter the Text to analyze............" mode={mode} />} />
+        <Route path="/about" element={<About />} />
+
+        
+        </Routes>
     </>
   );
 }
